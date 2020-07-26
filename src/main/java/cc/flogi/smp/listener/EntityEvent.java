2cbc805d020e9e0b1e56c2c2b0c04ca7d2ad6ef5
flogic <gamerking195@gmail.com>
2020-07-25 18:57:45 -0600

    Give creeper explosions 100% yield

--- src/main/java/cc/flogi/smp/listener/EntityEvent.java
+++ src/main/java/cc/flogi/smp/listener/EntityEvent.java
@@ -1,13 +1,16 @@
 package cc.flogi.smp.listener;
 
 import cc.flogi.smp.util.UtilThreading;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -27,6 +30,13 @@ public class EntityEvent implements Listener {
         }
     }
 
+    @EventHandler
+    public void onCreeperExplode(EntityExplodeEvent event) {
+        if (event.getEntity() instanceof Creeper) {
+            event.setYield(1);
+        }
+    }
+
     private void handleDragonDrops(EnderDragon dragon) {
         if (dragon.getDeathAnimationTicks() != 0) {
             UtilThreading.syncDelayed(() -> {
@@ -43,20 +53,4 @@ public class EntityEvent implements Listener {
             UtilThreading.syncDelayed(() -> handleDragonDrops(dragon), 60);
         }
     }
-
-//    @EventHandler
-//    public void onPhaseChange(EnderDragonChangePhaseEvent event) {
-//        EnderDragon dragon = event.getEntity();
-//        if (event.getNewPhase() == EnderDragon.Phase.DYING) {
-//            Bukkit.broadcastMessage("Delay - " + dragon.getDeathAnimationTicks());
-//            UtilThreading.syncDelayed(() -> {
-//                Bukkit.broadcastMessage("DROPPING SPECIAL ITEMS");
-//                dragon.getWorld().dropItem(dragon.getLocation(), new ItemStack(Material.ELYTRA));
-//                ((ExperienceOrb) dragon.getWorld().spawnEntity(dragon.getLocation(), EntityType.EXPERIENCE_ORB)).setExperience(1900);
-//                for (int i = 0; i < 10; i++) {
-//                    ((ExperienceOrb) dragon.getWorld().spawnEntity(dragon.getLocation(), EntityType.EXPERIENCE_ORB)).setExperience(960);
-//                }
-//            }, dragon.getDeathAnimationTicks());
-//        }
-//    }
 }
